a84edca8047b5d02637cbc3b2ee29b82e3b09a57
yeiner javier bejarano mora <javier@bluecaribu.com>
2021-12-09 11:57:06 -0500

    [inicio sesion outlook ]

---
+++ public/Callback.php
@@ -0,0 +1,35 @@
+<?php
+
+
+
+session_start(); 
+require "vendor/autoload.php";
+
+use myPHPnotes\Microsoft\Auth;
+use myPHPnotes\Microsoft\Handlers\Session;
+use myPHPnotes\Microsoft\Models\User;
+
+$microsoft = new Auth(Session::get("tenant_id"),Session::get("client_id"),  Session::get("client_secret"), Session::get("redirect_uri"), Session::get("scopes"));
+$tokens = $microsoft->getToken($_REQUEST['code'], Session::get("state"));
+
+// Setting access token to the wrapper
+$microsoft->setAccessToken($tokens->access_token);
+
+$user = (new User);
+
+
+if($user->data->getGivenName()){
+    $data=array(
+        "user"=>$user->data->getdisplayName(),
+        "id"=>$user->data->getid(),
+        "userPrincipalName"=>$user->data->getuserPrincipalName(),
+       
+    );
+    $data=serialize( $data);
+    $data= base64_encode($data);
+   
+    header("location:https://aulavirtual.ibero.edu.co/repositorio/sitios/estudiantes/?data= ". $data); 
+}
+
+
+?>
---
+++ public/Signin.php
@@ -0,0 +1,22 @@
+<?php
+
+session_start(); // Important
+require "vendor/autoload.php";
+
+use myPHPnotes\Microsoft\Auth;
+
+$tenant = "common"; 
+$client_id = "0d7b0a73-c487-443c-8ac1-32e9b959c5ab";
+$client_secret = "GcZ7Q~4GJ2FbUoT2QbQZ4FFsT36q3hZZr6yu1";
+$scopes = [
+            "User.Read",
+            'Files.Read.All',
+           
+        ];
+$callback = "http://localhost:8080/IBERO-Estudiantes/callback.php";
+//https://aulavirtual.ibero.edu.co/repositorio/sitios/estudiantes/callback.php
+
+$microsoft = new Auth($tenant, $client_id,  $client_secret, $callback, $scopes);
+header("location: ". $microsoft->getAuthUrl()); //Redirecting to get access token
+
+?>
